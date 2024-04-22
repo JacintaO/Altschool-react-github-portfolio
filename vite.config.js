8702ab319fa22d09defa66b935342e987e9b4647
JacintaO <jacinta.idialu@gmail.com>
2024-04-22 22:07:14 +0100

    vite config file fixed

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 export default defineConfig({
-  plugins: [react(), {
-    css: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
-    },
-  }],
-});
+  plugins: [react()],
+})
+
+
+// export default defineConfig({
+//   plugins: [react(), {
+//     css: {
+//       postcss: {
+//         plugins: [
+//           require('tailwindcss'),
+//           require('autoprefixer'),
+//         ],
+//       },
+//     },
+//   }],
+// });
